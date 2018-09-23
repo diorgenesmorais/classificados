fa0ca25a7aa5e22fa6a4c6916e4867c3a49b6b02
Diorgenes Morais <diorgenesmorais@gmail.com>
2018-09-23 00:42:47 -0300

    2ª aula

---
+++ pages/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Classificados</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="./" class="navbar-brand">Classificados</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Cadastre-se</a></li>
+          <li><a href="#">Login</a></li>
+        </ul>
+      </div>
+    </nav>
